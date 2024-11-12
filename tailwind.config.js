3b05f376ef4a16f96eabf0572572b0be12306233
Zeinab <zeinabansarian6280@gmail.com>
2024-11-12 14:44:31 +0330

    complete home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,16 +17,20 @@ module.exports = {
       xl: '1440px',
     },
     colors: {
-      'mainYellow':'#FEBE1B',
+      'main-pink':'#ED2289',
       'white':'#FFFFFF',
       'black':'#000000',
-      'gray-middle':'#828282',
-      'light-gray':'#F5F5F5',
+      'main-blue':'#194A96',
+      'dark-blue':'#002B43',
+      'new-gray':'#F2F2F2',
+      'light-gray':'#F2F2F2BD',
+      'too-light':'#FAFAFABD',
+      'lightbg':'#FAFAFA',
       'dark-gray':'#171717',
+      'gray-bg':'#F5F5F5',
       'dark3':'#636466',
       'dark2':'#111111',
       'gray2':'#A9A9A9'
-        
     },
     fontFamily: {
       'IRANSansWeb(FaNum)700': ['IRANSansWeb(FaNum)700', 'IRANSansWeb(FaNum)700'],
@@ -38,6 +42,8 @@ module.exports = {
       'IRANSansWeb(FaNum)700': ['"IRANSansWeb(FaNum)700"'],
       'IRANSansWeb(FaNum)400': ['"IRANSansWeb(FaNum)400"'],
       'Anton400': ['"Anton400"'],
+      'Battambang700': ['"Battambang700"'],
+      'BaiJamjuree700': ['"BaiJamjuree700"'],
 
     },
     extend: {
